c81809f29ac23ceee4d12bafc8f52f31150a6776
mingder wang <mingderwang@me.com>
2025-10-07 22:53:04 +0800

    fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,21 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   // for SSR or use "static" if only building static site
   output: "server",
-
+  build: {
+    minify: false, // Disable CSS minification
+  },
   adapter: vercel(),
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()],
+    css: {
+      // Disable LightningCSS optimization so it stops warning about @property
+      lightningcss: {
+        drafts: {
+          customProperties: true, // enable @property support
+        },
+      },
+    },
   },
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+import daisyui from "daisyui"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
